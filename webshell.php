156193b1f205872bec9e0950a508afe8b3e4e1c1
lamarsepi0l <132113497+lamarsepi0l@users.noreply.github.com>
2023-05-25 22:07:29 -0500

    Add disclaimer message

--- webshell.php
+++ webshell.php
@@ -1,3 +1,8 @@
+// This tool may be used for legal purposes only.  Users take full responsibility
+// for any actions performed using this tool.  The author accepts no liability
+// for damage caused by this tool.  If these terms are not acceptable to you, then
+// do not use this tool.
+
 <h2>Access Requested?</h2>
 <form method="POST" action="">
     <label for="password">Password:</label>
